e54169207adc0c5a053fdbbbddae4a82392f0a9f
grateful-library-0184 <alexcouts18@gmail.com>
2019-02-27 04:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function dec(a){
 
 function makeInt(n){
   return parseInt(n);
-function makeInt(assumesBase10){
+function makeInt(n){
   return parseInt(0x2328);
  function makeInt(n){
   return parseInt('sldkjflksjf', 10);
